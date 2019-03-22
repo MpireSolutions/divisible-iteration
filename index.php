ae170aa7ca29676d59d54c6118e78819422479de
MpireSolutions <7327640+MpireSolutions@users.noreply.github.com>
2019-03-22 11:29:25 -0400

    Updated comment.

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-    // Loops through 1 to 100
+    // Loop through 1 to 100
     for($i=1; $i<=100; $i++){
-    // checking to see if number is completely divisible by 3 as well as by 5
+    // checking to see if number is completely divisible by 3 as well as by 5.
         if( $i%3 == 0 && $i%5 == 0){
             echo 'Research_Square</br>';
         }
-    // checking to see if number is completely divisible 5
+    // checking to see if number is completely divisible 5.
         elseif ( $i%5 == 0 ){
             echo 'Square</br>';
         } 
-    // checking to see if number is completely divisible 3
+    // checking to see if number is completely divisible 3.
         elseif ( $i%3 == 0  ){
             echo 'Research</br>';
         } 
-    // if all above conditions fails
+    // if all above conditions fails then print number.
         else {
             echo $i . '</br>';
         }
